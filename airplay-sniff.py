d50703ee3da5feb264c53d622c039a0bde67d233
Stephen Shkardoon <ss23@ss23.geek.nz>
2015-03-17 17:30:43 +1300

    Change sniffing to be more robust
    Known bugs: Will update playing.txt when someone tries to use the speakers
    if already in use. Saving grace is it'll change to the right person a second later.

--- airplay-sniff.py
+++ airplay-sniff.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python2
 from scapy.all import *
 
+cur_ip = False
+
 def airplay_callback(pkt):
 	try:
-		if pkt['Raw'].load[0:5] == 'SETUP':
-			# Someone is starting to play! Add them to the list yo
-			with open('/tmp/playing.txt', 'w') as f:
-				f.write(pkt[IP].src)
-			print "Updated playing.txt to " + pkt[IP].src
-		elif pkt['Raw'].load[0:5] == 'TEARD':
-			# Someone is getting *off* those speakers, yo
-			with open('/tmp/playing.txt', 'w') as f:
-				pass # Rewrite it with nothing
-			print "Updated playing.txt to be blank"
+		if pkt[IP].sprintf('%proto%') == 'tcp':
+			# This could be anything! Parse further
+			if pkt['Raw'].load[0:5] == 'TEARD':
+				# Anyone can teardown, only remove the IP if it's the currently playing person
+				global cur_ip
+				if cur_ip == pkt[IP].src:
+					# Someone is getting *off* those speakers, yo
+					with open('/tmp/playing.txt', 'w') as f:
+						pass # Rewrite it with nothing
+					print "Updated playing.txt to be blank"
+		else:
+			# Should be UDP
+			if cur_ip != pkt[IP].src:
+				# A new person!
+				with open('/tmp/playing.txt', 'w') as f:
+					f.write(pkt[IP].src)
+				cur_ip = pkt[IP].src
+				print "Updated playing.txt to " + pkt[IP].src
 	except:
 		pass # meh
-sniff(filter="tcp and port 5000", store=0, prn=airplay_callback);
+sniff(filter="port 5000 or port 6001", store=0, prn=airplay_callback);
