5306943a391b16422ba84e7039366d969f9bf8bf
Elliot Sawyer <elliot@silverstripe.com>
2015-03-18 19:33:39 +1300

    more ways to identify the player via TCP requests

--- airplay-sniff.py
+++ airplay-sniff.py
@@ -2,19 +2,38 @@
 from scapy.all import *
 
 cur_ip = False
+def airplay_tcp(pkt):
+	if pkt['Raw'].load[0:5] == 'SETUP':
+		# Someone is starting to play! Add them to the list yo
+		with open('/var/tmp/playing.txt', 'w') as f:
+			f.write(pkt[IP].src)
+		print "Updated playing.txt to " + pkt[IP].src
+	elif pkt['Raw'].load[0:13] == 'SET_PARAMETER':
+		#someone is changing state of player (volume, track, etc)
+		with open('/var/tmp/playing.txt', 'w') as f:
+			f.write(pkt[IP].src)
+		print "Updated playing.txt to " + pkt[IP].src
+	elif pkt['Raw'].load[0:5] == 'FLUSH':
+		#someone is pausing playback
+		with open('/var/tmp/playing.txt', 'w') as f:
+			f.write(pkt[IP].src)
+		print "Updated playing.txt to " + pkt[IP].src
+	elif pkt['Raw'].load[0:5] == 'RECOR':
+		#someone is starting playback
+		with open('/var/tmp/playing.txt', 'w') as f:
+			f.write(pkt[IP].src)
+		print "Updated playing.txt to " + pkt[IP].src
+	elif pkt['Raw'].load[0:5] == 'TEARD':
+		# Someone is getting *off* those speakers, yo
+		with open('/var/tmp/playing.txt', 'w') as f:
+			pass # Rewrite it with nothing
+		print "Updated playing.txt to be blank"
 
 def airplay_callback(pkt):
 	try:
 		if pkt[IP].sprintf('%proto%') == 'tcp':
-			# This could be anything! Parse further
-			if pkt['Raw'].load[0:5] == 'TEARD':
-				# Anyone can teardown, only remove the IP if it's the currently playing person
-				global cur_ip
-				if cur_ip == pkt[IP].src:
-					# Someone is getting *off* those speakers, yo
-					with open('/tmp/playing.txt', 'w') as f:
-						pass # Rewrite it with nothing
-					print "Updated playing.txt to be blank"
+			#use TCP to id the ip
+			airplay_tcp(pkt)
 		else:
 			# Should be UDP
 			if cur_ip != pkt[IP].src:
@@ -25,4 +44,5 @@ def airplay_callback(pkt):
 				print "Updated playing.txt to " + pkt[IP].src
 	except:
 		pass # meh
+
 sniff(filter="port 5000 or port 6001", store=0, prn=airplay_callback);
